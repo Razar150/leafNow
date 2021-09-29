2f6fd537a49ef4c09e94f4dc03ea268a0f28fcf5
Razar150 <71868060+Razar150@users.noreply.github.com>
2021-09-29 11:15:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-3');
+let scrollTop = document.querySelector('.scroll-top');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 250){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+
+    if(window.scrollY > 250){
+        scrollTop.style.display = 'initial';
+    }else{
+        scrollTop.style.display = 'none';
+    }
+
+}
+
+var swiper = new Swiper(".home-slider", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop:true,
+});
+
+
+function openCity(evt, cityName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(cityName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+
+
+
